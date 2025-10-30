50eb07800c0fc3aaa773ba47aa0b734e273c9932
goal651 <bugiriwilson651@gmail.com>
2025-10-30 09:12:25 +0200

    fixed version errors

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,22 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const path = require("path");
 
-module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  // Merge aliases if already defined, or create a new one
-  config.resolver.alias = {
-    ...(config.resolver.alias || {}),
-    "@": path.resolve(__dirname),
-  };
+// Merge aliases if already defined, or create a new one
+config.resolver.alias = {
+  ...(config.resolver.alias || {}),
+  "@": path.resolve(__dirname),
+};
 
-  // Optional: Add file extensions if needed, to avoid resolution problems
-  config.resolver.sourceExts = [
-    ...(config.resolver.sourceExts || []),
-    "jsx",
-    "js",
-    "ts",
-    "tsx",
-    "json",
-  ];
+// Optional: Add file extensions if needed, to avoid resolution problems
+config.resolver.sourceExts = [
+  ...(config.resolver.sourceExts || []),
+  "jsx",
+  "js",
+  "ts",
+  "tsx",
+  "json",
+];
 
-  return config;
-})();
+module.exports = config;
